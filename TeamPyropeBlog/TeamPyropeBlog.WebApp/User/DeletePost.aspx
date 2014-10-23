f623cb07ef3415f2fba203bb23bc90efa7dd0a5d
flame78 <flame78@gmail.com>
2014-10-24 02:11:38 +0300

    add deletepost.aspx

---
+++ TeamPyropeBlog/TeamPyropeBlog.WebApp/User/DeletePost.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="DeletePost" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DeletePost.aspx.cs" Inherits="TeamPyropeBlog.WebApp.User.DeletePost" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>DELETE POST</h2>
+
+    
+
+</asp:Content>
---
+++ TeamPyropeBlog/TeamPyropeBlog.WebApp/User/DeletePost.aspx.cs
@@ -0,0 +1,23 @@
+﻿namespace TeamPyropeBlog.WebApp.User
+{
+    using System;
+    using System.Linq;
+    using System.Web.UI;
+
+    using TeamPyropeBlog.Data;
+
+    public partial class DeletePost : Page
+    {
+        private readonly PyropeBlogDbContext dbContext = new PyropeBlogDbContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string postIdParam = Request.Params["id"];
+            var postId = Int32.Parse(postIdParam);
+            var post = this.dbContext.Posts.SingleOrDefault(p => p.ID == postId);
+            this.dbContext.Posts.Remove(post);
+            this.dbContext.SaveChanges();
+            this.Response.Redirect("~/Default.aspx");
+        }
+    }
+}
